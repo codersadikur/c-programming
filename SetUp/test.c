df5823df334d15e7ceb18fae25d7f3fbe069a2de
codersadikur <codersadikur@gmail.com>
2023-04-01 19:30:19 +0600

    test.c

--- SetUp/test.c
+++ SetUp/test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    int a;
+    double b;
+    b = 65.6;
+    a = (int)b;
+    printf("the value of b is %0.2lf\n",b);
+    printf("the value of a is %d", a);
     return 0;
 }
